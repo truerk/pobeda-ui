a9be61f0f745f8e06b7daea101c64397d8d54852
Nickolay Eryomin <truerk@yandex.ru>
2020-06-15 11:07:08 +0400

    var in scss, export components

--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -1,8 +1,8 @@
 import tooltip from './tooltip.js'
 import dropdown from './dropdown.js'
 import modal from './ModalController.js'
-import select from './SelectController.js'
+import select, {SelectController} from './SelectController.js'
 import {ModalController} from './ModalController.js'
 import {ProgressController} from './ProgressController.js'
 
-export { tooltip, dropdown, modal, select, ModalController, ProgressController }
+export { tooltip, dropdown, modal, select, ModalController, ProgressController, SelectController }
