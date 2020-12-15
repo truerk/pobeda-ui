b10ce4c0754a18919ab09147d119199bcd572cb9
Nickolay Eryomin <truerk@yandex.ru>
2020-12-15 21:51:23 +0400

    gh-pages

---
+++ precache-manifest.43a39b4e000905a4d88fb574e47ab426.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055bbad0a64adbbd776dd75a6c6457c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc3f4fa21f58faf2e76",
+    "url": "/static/css/2.96ecccb1.chunk.css"
+  },
+  {
+    "revision": "820fed9282b2e86e37c4",
+    "url": "/static/css/main.4148fd02.chunk.css"
+  },
+  {
+    "revision": "abc3f4fa21f58faf2e76",
+    "url": "/static/js/2.595cfdb2.chunk.js"
+  },
+  {
+    "revision": "68c669cfc9c794b5c4f98b4f7e340be4",
+    "url": "/static/js/2.595cfdb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820fed9282b2e86e37c4",
+    "url": "/static/js/main.2d5ab33b.chunk.js"
+  },
+  {
+    "revision": "c27af932885fb41b30ffe7042c1fbdb4",
+    "url": "/static/js/main.2d5ab33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98121e96669ce22e5e3c",
+    "url": "/static/js/runtime-main.566bb99d.js"
+  }
+]);
